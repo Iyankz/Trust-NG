478dfca81d796c508de89c609023a1986b265359
Yayang Ardiansyah <39462527+Iyankz@users.noreply.github.com>
2024-10-16 14:35:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,17 @@ echo ""
 echo "----------------------------------------------------"
 echo "download file backup trust-ng"
 echo "----------------------------------------------------"
-wget http://download-trustng.resolver.id:40080/vzdump-qemu-dnstrustng-1.0.3.vma.zst -P /var/lib/vm/dump/
+wget http://download-trustng.resolver.id:40080/vzdump-qemu-dnstrustng-1.1.0.vma.zst -P /var/lib/vm/dump/
 
 echo "----------------------------------------------------"
 echo "download file sha1sum"
 echo "----------------------------------------------------"
-wget http://download-trustng.resolver.id:40080/vzdump-qemu-dnstrustng-1.0.3.vma.zst.sha1sum -P /var/lib/vm/dump/
+wget http://download-trustng.resolver.id:40080/vzdump-qemu-dnstrustng-1.1.0.vma.zst.sha1sum -P /var/lib/vm/dump/
 
 echo "----------------------------------------------------"
 echo "restore backup vm trust-ng"
 echo "----------------------------------------------------"
-qmrestore /var/lib/vz/dump/vzdump-qemu-dnstrustng-1.0.3.vma.zst 200 --storage local 
+qmrestore /var/lib/vz/dump/vzdump-qemu-dnstrustng-1.1.0.vma.zst 200 --storage local 
 
 echo "===================================================="
 echo " *** DONE *** "
